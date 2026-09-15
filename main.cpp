b6c58a2fcceda5527ab21a691d6db9a887002ae3
Atul Pandey <atulpandey.iitr@gmail.com>
2026-09-15 12:50:51 +0530

    Initialize project structure

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "ATLAS_Autonomous_Transit_Coordination_and_Routing_Engine" << std::endl;
+    std::cout << "Project initialized successfully." << std::endl;
+
+    return 0;
+}
